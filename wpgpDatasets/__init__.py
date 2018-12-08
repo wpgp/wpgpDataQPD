86e19e04efb74411da4a80034be0f29f3892f175
Nikos Ves <vesnikos@users.noreply.github.com>
2018-12-08 23:49:34 +0000

    Update __init__.py

--- wpgpDatasets/__init__.py
+++ wpgpDatasets/__init__.py
@@ -1,1 +1,16 @@
-# -*- coding: utf-8 -*-__author__ = 'Nikolaos Ves'__email__ = 'vesnikos@gmail.com'__version__ = '0.1.0'from qgis.gui import QgisInterface# noinspection PyPep8Namingdef classFactory(iface: QgisInterface):  # pylint: disable=invalid-name    """Load WpDatasets class from file WpDatasets."""    # type iface: QgsInterface    from .wp_datasets import WpDatasets    return WpDatasets(iface)
+# -*- coding: utf-8 -*-
+__author__ = 'Nikolaos Ves'
+__email__ = 'vesnikos@gmail.com'
+__version__ = '0.1.0'
+
+from qgis.gui import QgisInterface
+
+
+# noinspection PyPep8Naming
+def classFactory(iface: QgisInterface):  # pylint: disable=invalid-name
+    """Load WpDatasets class from file WpDatasets."""
+
+    # type iface: QgsInterface
+    from .wp_datasets import WpDatasets
+    return WpDatasets(iface)
+  
